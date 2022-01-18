6facf8a99466a46ee442087a0d014ff623a215d0
v.yurin <v.yurin@8M-1CREPORT>
2022-01-18 14:46:42 +0300

    Новый

---
+++ ig_intranet/Models/UsersImp.cs
@@ -0,0 +1,15 @@
+﻿
+namespace EnumSample.Models
+{
+    public class UsersImp
+    {
+        public int Id { get; set; }
+        public string User_FIO { get; set; }
+        public string User_FIO_Aunt { get; set; }
+        public string Domen { get; set; }
+        public string User_in_Domen { get; set; }        
+        public string User_UID { get; set; }
+        public int FlagDel { get; set; }
+        public string Date_add_user { get; set; }
+    }
+}
---
+++ ig_intranet/Models/UsersImpContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EnumSample.Models
+{
+    
+    public class UsersImpContext : DbContext
+    {
+        public DbSet<UsersImp> UsersImp { get; set; }
+        
+    }
+}
